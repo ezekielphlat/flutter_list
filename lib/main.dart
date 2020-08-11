7a50e173624621435aea7249430a5558a0cd597c
Ezekiel <ezekielphlat@gmail.com>
2020-08-12 00:54:43 +0100

    get data from a custom class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() =>
   runApp(MaterialApp(
@@ -11,10 +12,10 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
-  List<String> quotes = [
-    'Be yourself; everyone else is already taken',
-    'I have nothing to declare except my genius',
-    'The truth is rarely pure and never simple.'
+  List<Quote> quotes = [
+    Quote(author: 'Wole shoyinka',text:'Be yourself; everyone else is already taken'),
+    Quote(author: 'Chino achebe', text: 'I have nothing to declare except my genius' ),
+    Quote(author: 'Ezekiel Abode', text: 'The truth is rarely pure and never simple' )
 
   ];
   @override
@@ -27,7 +28,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body:Column(
-        children: quotes.map((quote)=>Text(quote)).toList(),
+        children: quotes.map((quote)=>Text('${quote.text} - ${quote.author} ')).toList(),
       )
     );
   }
---
+++ lib/quote.dart
@@ -0,0 +1,9 @@
+class Quote {
+  String text;
+  String author;
+
+  //make the constructor a named parameter. and assign property automatically
+  Quote({this.text, this.author});
+
+
+}
